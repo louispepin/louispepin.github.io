0deaab924a0a15e57bcffcfdbd2d1a57d564ae19
louispepin <louis.pepin.ce@gmail.com>
2014-09-16 09:58:08 -0400

    Webserver test 3 wtf

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-/**
- * Created by Louis on 9/15/2014.
- */
+angular.module('app', [
+    'ngRoute',
+    'app.home',
+    'app.snake',
+    'app.asteroid'
+])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'dummy.html',
+            controller: 'homeCtrl'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
+
+angular.module('app.home', [])
+.controller('homeCtrl', ['$scope', function($scope) {
+    $scope.text = "Home";
+}]);
+
+angular.module('app.snake', [])
+.controller('snakeCtrl', ['$scope', function($scope) {
+    $scope.text = "Snake";
+}]);
+
+angular.module('app.asteroid', [])
+.controller('asteroidCtrl', ['$scope', function($scope) {
+    $scope.text = "Asteroid";
+}]);
